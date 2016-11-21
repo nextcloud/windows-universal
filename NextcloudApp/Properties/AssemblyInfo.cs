19d78aa76003a79390962ec912458a7f72e78fe8
Fiedler, Andre <afiedler@valuephone.com>
2016-11-21 21:56:54 +0100

    new app name

--- NextcloudApp/Properties/AssemblyInfo.cs
+++ NextcloudApp/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nextcloud App")]
+[assembly: AssemblyTitle("Nextcloud")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nextcloud App")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Nextcloud")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
