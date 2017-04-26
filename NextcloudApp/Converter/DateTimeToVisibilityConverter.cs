44ba625d9ec1bf0e08deab0e7477242b1533d72a
DecaTec <jr@decatec.de>
2017-04-26 21:47:46 +0200

    Moved ETag at the end of the list; creation date is now hidden when it's DateTime.MinValue

---
+++ NextcloudApp/Converter/DateTimeToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace NextcloudApp.Converter
+{
+    public class DateTimeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dt = (DateTime)value;
+            return dt.Equals(DateTime.MinValue) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
