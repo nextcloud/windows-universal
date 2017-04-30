35336eb1cb13228f1c1fbc9e7963dfedcefa4b5d
DecaTec <jr@decatec.de>
2017-04-30 10:19:07 +0200

    Stop sync this folder is disabled in the command bar when the current folder is not synced

--- NextcloudApp/Converter/IsThisFolderSyncedToIsEnabledConverter.cs
+++ NextcloudApp/Converter/IsThisFolderSyncedToIsEnabledConverter.cs
@@ -2,8 +2,6 @@
 using NextcloudApp.Utils;
 using NextcloudClient.Types;
 using System;
-using Windows.ApplicationModel.Resources;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace NextcloudApp.Converter
@@ -11,27 +9,27 @@ namespace NextcloudApp.Converter
     /// <summary>
     /// Checks if the current folder is synchronized or not
     /// </summary>
-    public class IsThisFolderSyncedConverter : IValueConverter
+    public class IsThisFolderSyncedToIsEnabledConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var item = DirectoryService.Instance.PathStack.Count > 0 ? DirectoryService.Instance.PathStack[DirectoryService.Instance.PathStack.Count - 1].ResourceInfo : null;
 
             if (item == null)
-                return Visibility.Visible;
+                return false;
 
             if (item.ContentType == null || !item.ContentType.Equals("dav/directory"))
             {
-                return Visibility.Collapsed;
+                return false;
             }
 
             if (SyncDbUtils.IsSynced(item))
             {
-                return Visibility.Visible;
+                return true;
             }
             else
             {
-                return Visibility.Collapsed;
+                return false;
             }
         }
 
