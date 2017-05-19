18f6e9d1e5d2a4d7f5fab27085b167ee5777f29f
DecaTec <jr@decatec.de>
2017-05-19 19:28:22 +0200

    Position of MenuFlyout

--- NextcloudApp/Actions/OpenMenuFlyoutAction.cs
+++ NextcloudApp/Actions/OpenMenuFlyoutAction.cs
@@ -1,6 +1,8 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls.Primitives;
 using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
 
 namespace NextcloudApp.Actions
 {
@@ -9,9 +11,10 @@ namespace NextcloudApp.Actions
         public object Execute(object sender, object parameter)
         {
             var senderElement = sender as FrameworkElement;
+            var args = parameter as RightTappedRoutedEventArgs;
             var flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement);
-
-            flyoutBase.ShowAt(senderElement);
+            var menuFlyout = flyoutBase as MenuFlyout;
+            menuFlyout.ShowAt(senderElement, args.GetPosition(senderElement));
 
             return null;
         }
