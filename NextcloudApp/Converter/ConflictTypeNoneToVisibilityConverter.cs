cc378003e762b4503a72fb286869e6a9a1fe1198
SunboX <andre.fiedler@me.com>
2017-06-08 22:09:10 +0200

    layout changes (WIP)

---
+++ NextcloudApp/Converter/ConflictTypeNoneToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using NextcloudApp.Models;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace NextcloudApp.Converter
+{
+    public class ConflictTypeNoneToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var conflictType = (ConflictType) value;
+            var inverse = parameter != null && bool.Parse(parameter.ToString());
+            
+            if (inverse)
+            {
+                return conflictType == ConflictType.None ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return conflictType == ConflictType.None ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
